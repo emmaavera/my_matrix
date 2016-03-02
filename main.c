0bc279fda195a7d8d1c101b2a98c6e13a9b54de7
emmaavera <emma.avera.stuy@gmail.com>
2016-03-02 01:22:01 -0500

    first

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+#include "matrix.h"
+
+int main() {
+
+  screen s;
+  struct matrix *edges;
+  struct matrix *transform;
+
+  edges = new_matrix(4, 4);
+  pop(edges);
+  transform = new_matrix(4, 4);
+  pop(transform);
+
+  printf("Testing that edges has been initialized and\npopulated so that m[i][j] = 1...\n");
+  print_matrix(edges);
+
+  
+  printf("\nTesting ident on transform...\n");
+  ident(transform);
+  print_matrix(transform);
+
+  printf("\nTesting scalar_mult on edges..\n");
+  printf("times 3\n");
+  scalar_mult(3, edges);
+  print_matrix(edges);
+
+  free_matrix( transform );
+  free_matrix( edges );
+}  
